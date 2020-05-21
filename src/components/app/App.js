0e64d7d07356897b22e2af80af9a91a8504066c3
Cmlsltnl <cmlsltnl@gmail.com>
2020-05-22 01:39:57 +0400

    Update App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -9,8 +9,8 @@ class App extends Component {
     return (
       <div className="app">
         <header className="app-header">
-          <h1 className="app-title">Would you still be alive?</h1>
-          <h2 className="app-description">Farmed animals are killed at a very young age. If you were one, would you still be alive? Find the answer.</h2>
+          <h1 className="app-title">Əgər bir ferma heyvanı olsaydın hələ də yaşayardın?</h1>
+          <h2 className="app-description">Ferma heyvanları hələ həyatlarını tam yaşamamış öldürülürlər. Əgər bir ferma heyvanı olsaydın səncə indi aqibətin necə idi?</h2>
         </header>
         <div className="app-core">
           <Age/>
